1981fa40e67cf1be4b3e0436f364774515902ee4
greenbalcony <info.greenbalcony@gmail.com>
2024-09-20 11:18:34 +0000

    Updated CORS and URL configuration

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.getElementById('plant-form').addEventListener('submit', function(event)
 });
 
 function identifyPlant(imageFile) {
-  const apiUrl = 'https://plant-identifier-app-private-glxv.vercel.app/identify'; // Updated for Vercel
+  const apiUrl = 'https://plant-identifier-hdigg3z4b-brunos-projects-e594ffb4.vercel.app/identify'; // Updated for Vercel
 
   const formData = new FormData();
   formData.append('organ', 'auto');
